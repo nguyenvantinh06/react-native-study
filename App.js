c33a68cf41c26aa3182667783ea5e487a1ca3b99
Nguyễn Văn Tĩnh <nguyenvantinh06@gmail.com>
2021-09-13 01:51:17 +0700

    + Core Components and Native Components

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Image, TextInput, ScrollView } from 'react-native';
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hello, world!</Text>
-    </View>
+    <ScrollView>
+      <Text>Some text</Text>
+      <Text>Some more text</Text>
+      <Image 
+        source={{uri:'https://reactnative.dev/docs/assets/p_cat2.png'}}
+        style={{width:200, height: 200}}
+      />
+      <TextInput
+        style={styles.textInput}
+        placeholder="You can type in me"
+      />
+    </ScrollView>
   );
 }
 
@@ -14,6 +23,13 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  textInput: {
+    height: 40,
+    borderColor: 'grey',
+    borderWidth: 1,
+    marginHorizontal: 5,
+    paddingHorizontal: 5
   }
 })
 
