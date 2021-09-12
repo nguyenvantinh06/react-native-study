46db28ca409f0a1338208bb37fe11ae1662a85aa
Nguyễn Văn Tĩnh <nguyenvantinh06@gmail.com>
2021-09-13 02:55:18 +0700

    +Add React Fundamentals (Components, JSX, props, state)

--- App.js
+++ App.js
@@ -1,35 +1,61 @@
-import React from 'react';
-import { Text, View, StyleSheet, Image, TextInput, ScrollView } from 'react-native';
+import React, {useState} from 'react';
+import { Text, View, StyleSheet, Image, TextInput, ScrollView, Button } from 'react-native';
+
+const getFullName = (firstName, lastName) =>{
+  return firstName + ' ' + lastName;
+}
+const Cat = (props) => {
+  const [isHungry, setIsHungry] = useState(true);
+  return (
+    <View style={{alignItems: 'center'}}>
+      <Text>
+        I am {props.name}, and I am {isHungry ? "hungry" : "full"}!
+      </Text>
+      <Button
+        onPress={()=>{
+          setIsHungry(false);
+        }}
+        disabled={!isHungry}
+        title={isHungry? "Pour me some milk, please!" : "Thank you!"}
+      />
+    </View>
+    
+  );
+}
 
 const App = () => {
   return (
-    <ScrollView>
-      <Text>Some text</Text>
-      <Text>Some more text</Text>
-      <Image 
-        source={{uri:'https://reactnative.dev/docs/assets/p_cat2.png'}}
-        style={{width:200, height: 200}}
-      />
-      <TextInput
-        style={styles.textInput}
-        placeholder="You can type in me"
-      />
-    </ScrollView>
+    <>
+      <ScrollView>
+        <Text>Some text</Text>
+        <Text>Some more text</Text>
+        <Image 
+          source={{uri:'https://reactnative.dev/docs/assets/p_cat2.png'}}
+          style={{width:200, height: 200}}
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholder="You can type in me"
+        />
+        <Text>Hello, I am {getFullName("Tinh","Nguyen")}</Text>
+        <Cat name="Munkustrap" />
+        <Cat name="Spot" />
+      </ScrollView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
   },
   textInput: {
     height: 40,
     borderColor: 'grey',
     borderWidth: 1,
     marginHorizontal: 5,
-    paddingHorizontal: 5
+    paddingHorizontal: 5,
+    marginVertical: 5,
   }
 })
 
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # react-native-study
+
+Props is short for “properties”. Props let you customize React components. For example, here you pass each <Cat> a different name for Cat to render.
+
+Notice the double curly braces {{ }} surrounding style‘s width and height. In JSX, JavaScript values are referenced with {}. This is handy if you are passing something other than a string as props, like an array or number: <Cat food={["fish", "kibble"]} age={2} />. However, JS objects are also denoted with curly braces: {width: 200, height: 200}. Therefore, to pass a JS object in JSX, you must wrap the object in another pair of curly braces: {{width: 200, height: 200}}
+
+State#
+While you can think of props as arguments you use to configure how components render, state is like a component’s personal data storage. State is useful for handling data that changes over time or that comes from user interaction. State gives your components memory!
+
+As a general rule, use props to configure a component when it renders. Use state to keep track of any component data that you expect to change over time.
+
+You can add state to a component by calling React’s useState Hook. A Hook is a kind of function that lets you “hook into” React features. For example, useState is a Hook that lets you add state to function components. You can learn more about other kinds of Hooks in the React documentation.
